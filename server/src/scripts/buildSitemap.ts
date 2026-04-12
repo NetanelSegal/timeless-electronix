e62da304f548be1cff1022269dc2ff8cced907ee
netanel segal <segal.netanel4@gmail.com>
2026-04-12 09:34:35 +0300

    up

--- server/src/scripts/buildSitemap.ts
+++ server/src/scripts/buildSitemap.ts
@@ -1,17 +1,17 @@
-import fs from "node:fs";
-import path from "node:path";
-import mongoose from "mongoose";
-import { env } from "../config/env.js";
-import { buildSitemapXmlString } from "../utils/sitemap.js";
+import fs from 'node:fs';
+import path from 'node:path';
+import mongoose from 'mongoose';
+import { env } from '../config/env.js';
+import { buildSitemapXmlString } from '../utils/sitemap.js';
 
-const outPath = path.resolve(import.meta.dirname, "../../dist/sitemap.xml");
+const outPath = path.resolve(process.cwd(), 'dist/sitemap.xml');
 
 async function main() {
-  console.log("buildSitemap: connecting to MongoDB...");
+  console.log('buildSitemap: connecting to MongoDB...');
   await mongoose.connect(env.MONGODB_URI);
   const xml = await buildSitemapXmlString(env.PUBLIC_SITE_URL);
   fs.mkdirSync(path.dirname(outPath), { recursive: true });
-  fs.writeFileSync(outPath, xml, "utf8");
+  fs.writeFileSync(outPath, xml, 'utf8');
   await mongoose.disconnect();
   console.log(`buildSitemap: wrote ${outPath}`);
 }
